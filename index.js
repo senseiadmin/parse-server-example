5e57e6dade7ff2b69dab3c9e02df677fbf46c7b8
Big-Timmy <temiotu47@tamu.edu>
2021-09-25 19:38:46 -0500

    updated index

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 const config = {
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb+srv://parse:Password1@cluster0.am4qf.mongodb.net/test',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'sensei_dev',
+  masterKey: process.env.MASTER_KEY || 'tempk', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
