a2662b7f013cd4130ca1b0ada53afe8808038c6a
Big-Timmy <temiotu47@tamu.edu>
2021-09-27 01:32:54 -0500

    updated env vars

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ if (!databaseUri) {
 const config = {
   databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'sensei_dev',
-  masterKey: process.env.MASTER_KEY || 'tempk', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
