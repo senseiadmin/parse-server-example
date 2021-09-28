316bf45790bc93cc5fea057e97167c2de50e5f46
Big-Timmy <temiotu47@tamu.edu>
2021-09-28 01:39:06 -0500

    updated env vars

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const config = {
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+  readOnlyMasterKey: process.env.READ_ONLY_MASTER_KEY,
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
